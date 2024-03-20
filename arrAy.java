c4c578783373e249507c45951e1d2706f3184ead
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-20 11:10:45 +0400

    Add files via upload

---
+++ arrAy.java
@@ -0,0 +1,18 @@
+public class arrAy{
+  public static void main (String[] arg){
+    int [] arr={2, 5, 8, 7, 10};
+    int count=0;
+    for (int i=0; i<arr.length; i++){
+      if (arr[i]%2==0){
+        count++;
+      }
+     
+         
+    }
+  
+   System.out.println("Odd number array: "+ (arr.length-count));
+      
+   System.out.println("Even number array: "+ count);
+  
+  }
+}
